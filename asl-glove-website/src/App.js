8a54825a68fa1b2681aec9857abb21c840964e66
goldenxuett <ttanpcs@gmail.com>
2022-05-01 00:40:04 -0500

    Create initial react template

---
+++ asl-glove-website/src/App.js
@@ -0,0 +1,37 @@
+import "./styles.sass";
+
+function App() {
+  return (
+    <div className = "background">
+    <div className = "content">
+    <div className = "title-box">
+      American Sign Language Translation Glove
+    </div>
+    <div className = "title-box">
+      About
+    </div>
+    <div className = "title-box">
+      Plan
+    </div>
+    <div className = "title-box">
+      Members
+    </div>
+    </div>      
+
+    <div className = "footer">
+        <div className = "text">
+        Copyright © 2022 Tony Tan
+        </div>
+        <div className = "text">
+        ttanpcs@gmail.com
+        </div>
+        <div className = "text">
+        github.com/goldenxuett
+        </div>
+    </div>   
+</div>
+
+  )
+}
+
+export default App;
