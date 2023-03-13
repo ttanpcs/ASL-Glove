4476e65809fe8aa80e41e901ba50510c2fec696b
goldenxuett <ttanpcs@gmail.com>
2023-03-13 17:25:29 -0500

    Implement Mode based arduino code

--- test/test.ino
+++ test/test.ino
@@ -1,18 +1,24 @@
 #include "Arduino_BMI270_BMM150.h"
 #include <ArduinoJson.h>
 
+enum class Mode { Idle, Write, Snapshot };
+Mode current_mode;
+bool is_start;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   while (!IMU.begin());
+  current_mode = Mode::Idle;
+  is_start = true;
 }
 
-void loop() {
+void readSensors() {
   float ax, ay, az;
   float gx, gy, gz;
-  StaticJsonDocument<200> doc;
   StaticJsonDocument<200> im;
   while (!IMU.accelerationAvailable());
+
   IMU.readAcceleration(ax, ay, az);
   im["ax"] = ax;
   im["ay"] = ay;
@@ -23,6 +29,14 @@ void loop() {
   im["gx"] = gx;
   im["gy"] = gy;
   im["gz"] = gz;
+
+  char imBuffer[2048];
+  serializeJson(im, imBuffer);
+  Serial.print(imBuffer);
+}
+
+void readVoltages() {
+  StaticJsonDocument<200> doc;
   
   doc["A0"] = analogRead(A0);
   doc["A1"] = analogRead(A1);
@@ -32,10 +46,27 @@ void loop() {
   doc["A7"] = analogRead(A7);
 
   char jsonBuffer[2048];
-  char imBuffer[2048];
   serializeJson(doc, jsonBuffer);
-  serializeJson(im, imBuffer);
-  Serial.println(jsonBuffer);
-  Serial.println(imBuffer);
+  Serial.print(jsonBuffer);
+}
+
+void loop() {
+  if (Serial.available()) {
+    Mode read_mode = static_cast<Mode>(Serial.parseInt());
+    if (current_mode == Mode::Idle && read_mode != Mode::Idle) {
+      is_start = true;
+    }
+    current_mode = read_mode;
+  }
+  if (current_mode != Mode::Idle) {
+    Serial.print(is_start);
+    readSensors();
+    readVoltages();
+    Serial.println();
+    if (current_mode == Mode::Snapshot) {
+      current_mode = Mode::Idle;
+    }
+    is_start = false;
+  }
 }
 
